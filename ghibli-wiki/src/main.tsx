833478c16c345087bcde15c3a82af95a8402aebd
Daniel Perez <danielperezf98@gmail.com>
2023-12-13 13:35:19 -0800

    added basename to browser router

--- ghibli-wiki/src/main.tsx
+++ ghibli-wiki/src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='https://daniel-perezf.github.io/ghibli-wiki/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
